a44b3dac5e5eeef10b9ae08a242ca45b275df4df
aurasoph <aurasoph@uw.edu>
2025-08-19 14:41:18 -0700

    fixed organization

--- lakefile.lean
+++ lakefile.lean
@@ -12,6 +12,8 @@ require "mathlib" from git
 meta if get_config? env = some "dev" then -- dev is so not everyone has to build it
 require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "main"
 
+lean_lib QuantumCircuits
+
 lean_lib ClassicalInfo
 
 lean_lib StatMech
